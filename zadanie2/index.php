fa8111b5860fc49a4f44a18437cda980c68ecdbe
Wojtek <wojtek.lenartowicz@gmail.com>
2015-09-25 16:56:07 +0200

    Made an extensed version of lotto simulator. Former 'index.php' has became 'lotto.php' and current 'index.php' is a page that lets you choose the size of available numbers's array.

--- zadanie2/index.php
+++ zadanie2/index.php
@@ -1,48 +1,3 @@
-<?php
-$lottoStart = 0;
-$lottoEnd = 48;
-
-function Generate($start, $end){
-    $tab = array();
-    for($i = $start+1; $i <= $end+1; $i++){
-        array_push($tab, $i);
-    }
-    return $tab;
-}
-
-$chosenNumbers = array(); // numbers chosen by user
-
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    for($i = $lottoStart+1; $i <= $lottoEnd+1; $i++){
-        if($_POST["check$i"] == 'on'){
-            array_push($chosenNumbers, $i);
-        }
-    }
-    if(count($chosenNumbers) != 6){
-        $chosenNumbers = array();
-        $error = 'Wybierz dokładnie 6 liczb.';
-    }
-    else{
-        $error = '';
-        $generatedTab = Generate($lottoStart, $lottoEnd); // generated tab of numbers that can be chosen
-        shuffle($generatedTab);
-        $slicedTab = array_slice($generatedTab, 0, 6, true);
-        sort($slicedTab);
-        $numDisplay = ''; // numbers to display at the end
-        $count = 0; // counting good guesses
-        foreach($chosenNumbers as $chNum){
-            if(in_array($chNum, $slicedTab)){
-                $numDisplay .= "<font color=\"green\">".$chNum."</font> ";
-                $count++;
-            }
-            else{
-                $numDisplay .= $chNum." ";
-            }
-        }
-    }
-}
-?>
-
 <!DOCTYPE html>
 <html>
     <head>
@@ -50,43 +5,14 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
         <meta charset="UTF-8">
     </head>
     <body>
-        <form action="index.php" method="POST">
-            <legend>Symulator Lotto:</legend>
+        <form action="lotto.php" method="GET">
+            <legend>Podaj przedział liczb w losowaniu:</legend>
             <fieldset>
-                <table border="1">
-                    <?php
-                    for($i = $lottoStart; $i <= $lottoEnd; $i++){
-                        if($i % 10 == 0){
-                            echo("<tr>");
-                        }
-                        echo(
-                            "<td>"
-                                . "<input type='checkbox' name='check".($i+1)."'>" . ($i + 1) .
-                            "</td>"
-                        );
-                        if(($i + 1) % 10 == 0){
-                            echo("</tr>");
-                        }
-                    }
-                    ?>
-                </table>
+                <input type="text" name="start" placeholder="początkowa liczba">
+                <input type="text" name="end" placeholder="końcowa liczba">
                 <br>
-                <button type="submit">Losuj</button>
+                <button type="submit">Ustaw</button>
             </fieldset>
         </form>
-        <?php
-        if(strlen($error) > 0){
-            echo($error);
-        }
-        if(trim(strlen($numDisplay)) > 0){
-            echo("<p>Wylosowane: ");
-            foreach($slicedTab as $num){
-                echo("$num ");
-            }
-            echo("<br>Twoje: ");
-            echo("$numDisplay");
-            echo("<br>Ilość trafień: $count</p>");
-        }
-        ?>
     </body>
 </html>
---
+++ zadanie2/lotto.php
@@ -0,0 +1,92 @@
+<?php
+$lottoStart = $_GET['start']-1;
+$lottoEnd = $_GET['end']-1;
+
+function Generate($start, $end){
+    $tab = array();
+    for($i = $start+1; $i <= $end+1; $i++){
+        array_push($tab, $i);
+    }
+    return $tab;
+}
+
+$chosenNumbers = array(); // numbers chosen by user
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    for($i = $lottoStart+1; $i <= $lottoEnd+1; $i++){
+        if($_POST["check$i"] == 'on'){
+            array_push($chosenNumbers, $i);
+        }
+    }
+    if(count($chosenNumbers) != 6){
+        $chosenNumbers = array();
+        $error = 'Wybierz dokładnie 6 liczb.';
+    }
+    else{
+        $error = '';
+        $generatedTab = Generate($lottoStart, $lottoEnd); // generated tab of numbers that can be chosen
+        shuffle($generatedTab);
+        $slicedTab = array_slice($generatedTab, 0, 6, true);
+        sort($slicedTab);
+        $numDisplay = ''; // numbers to display at the end
+        $count = 0; // counting good guesses
+        foreach($chosenNumbers as $chNum){
+            if(in_array($chNum, $slicedTab)){
+                $numDisplay .= "<font color=\"green\">".$chNum."</font> ";
+                $count++;
+            }
+            else{
+                $numDisplay .= $chNum." ";
+            }
+        }
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Symulator Lotto.</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <form action="#" method="POST">
+            <legend>Symulator Lotto:</legend>
+            <fieldset>
+                <table border="1">
+                    <?php
+                    for($i = $lottoStart; $i <= $lottoEnd; $i++){
+                        if($i % 10 == 0){
+                            echo("<tr>");
+                        }
+                        echo(
+                            "<td>"
+                                . "<input type='checkbox' name='check".($i+1)."'>" . ($i + 1) .
+                            "</td>"
+                        );
+                        if(($i + 1) % 10 == 0){
+                            echo("</tr>");
+                        }
+                    }
+                    ?>
+                </table>
+                <br>
+                <button type="submit">Losuj</button>
+            </fieldset>
+        </form>
+        <?php
+        if(strlen($error) > 0){
+            echo($error);
+        }
+        if(trim(strlen($numDisplay)) > 0){
+            echo("<p>Wylosowane: ");
+            foreach($slicedTab as $num){
+                echo("$num ");
+            }
+            echo("<br>Twoje: ");
+            echo("$numDisplay");
+            echo("<br>Ilość trafień: $count</p>");
+        }
+        ?>
+    </body>
+</html>
